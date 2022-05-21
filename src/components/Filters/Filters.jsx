e8da248cd9b7a7d02866f1d1fe64f2b97273017d
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-05-21 15:19:52 -0300

    add filter

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -5,13 +5,12 @@ import './Filters.css';
 import { getGenres } from '../../utils/api';
 
 const Filters = ({ movies, setMovies }) => {
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState(0);
   const [filters, setFilters] = useState([]);
 
   const fetchGenres = async () => {
     const data = await getGenres();
     setFilters(data.genres);
-    // console.log(data.genres);
   };
 
   useEffect(() => {
@@ -21,10 +20,10 @@ const Filters = ({ movies, setMovies }) => {
   useEffect(() => {
     if (filter) {
       const movieFilter = movies.filter(
-        (movie) => movie.genre_ids.id === filter
+        (movie) => movie.genre_ids[1] === Number(filter)
       );
       console.log(movieFilter);
-      //   setMovies(movieFilter);
+      setMovies(movieFilter);
     }
   }, [filter]);
 
@@ -33,16 +32,12 @@ const Filters = ({ movies, setMovies }) => {
       <h2>Filtrar por :</h2>
       <form className="filters__form">
         <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-          <option value="">Genero</option>
+          <option>Genero</option>
           {filters.map((filter) => (
-            <option value={filter.id} key={filter.name}>
+            <option value={filter.id} key={filter.id}>
               {filter.name}
             </option>
           ))}
-        </select>
-        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-          <option value="">Mas actual</option>
-          <option value="">Mas antigua</option>
         </select>
       </form>
     </div>
